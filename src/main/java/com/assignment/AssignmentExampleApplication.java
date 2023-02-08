9d4f975d82f6cfa14364014ce6c327160bb6f9f8
Jadhav-nikhil <nikhiljadhavp64@example.com>
2023-02-08 12:16:18 +0530

    log statement added

--- src/main/java/com/assignment/AssignmentExampleApplication.java
+++ src/main/java/com/assignment/AssignmentExampleApplication.java
@@ -1,13 +1,16 @@
 package com.assignment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AssignmentExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentExampleApplication.class, args);
+		log.info("Spring boot Application started ");
 	}
 
 }
